ab81c00e4246dcd0b7c369014e31d5246c068b35
Grig Alex <alexengrigdev@gmail.com>
2020-05-16 14:08:21 +0300

    Create spring-boot project

---
+++ wt-lab-3/src/main/java/dev/alexengrig/suai/wtlab3/WtLab3Application.java
@@ -0,0 +1,11 @@
+package dev.alexengrig.suai.wtlab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtLab3Application {
+  public static void main(String[] args) {
+    SpringApplication.run(WtLab3Application.class, args);
+  }
+}
---
+++ wt-lab-3/src/test/java/dev/alexengrig/suai/wtlab3/WtLab3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexengrig.suai.wtlab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtLab3ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
